a2bbb09c51a279356cae886a112954014e9c8362
luck22y <luckykumar2204@gmail.com>
2023-10-11 11:49:53 +0530

    init commit

---
+++ src/components/ProfileIcon.jsx
@@ -0,0 +1,10 @@
+import { TbGridDots } from "react-icons/tb";
+
+import Profile from "../assets/Icon-red.png";
+import ProfileRing from "../assets/profile-ring.svg";
+
+const ProfileIcon = () => {
+    return <div>Profile Icon</div>;
+};
+
+export default ProfileIcon;
