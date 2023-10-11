a02923a4d9fa011071d8f2ffab8f84c78e274cd7
luck22y <luckykumar2204@gmail.com>
2023-10-11 15:30:12 +0530

    Home Section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,29 @@ import HomeHeader from "./HomeHeader";
 import SearchInput from "./SearchInput";
 import Footer from "./Footer";
 const Home = () => {
-    return <div>Home</div>;
+  return (
+    <div className="flex flex-col h-[100vh]">
+      <HomeHeader />
+      <main className="grow flex justify-center">
+        <div className="w-full px-5 flex flex-col items-center mt-44">
+          <img
+            className="w-[172px] md:w-[272px] mb-8"
+            src={Logo}
+            alt="Google Search"
+          />
+          <SearchInput />
+          <div className="flex gap-2 mt-8 text-[#3c4043]">
+            <button className="h-9 px-4 bg-[#f8f9fa] text-sm rounded-md border border-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2">
+              Google Search
+            </button>
+            <button className="h-9 px-4 bg-[#f8f9fa] text-sm rounded-md border border-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2">
+              I'm Feeling Lucky
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
